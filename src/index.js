640f2f9f923bae1a05d35c65b77b401ff0816197
rowland henshaw <rowland.henshaw@andela.com>
2018-05-05 15:20:29 +0100

    restored lambda

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// import config from './config';
+import config from './config';
 
 const welcomeMessage = {
   message: 'Welcome to aws lambda'
@@ -11,6 +11,6 @@ const invoke = cb => cb(null, { welcomeMessage });
 module.exports.handler = (event, context, cb) => {
   const { accessKey } = event;
 
-  // if (accessKey === apiKey) return invoke(cb);
+  if (accessKey === apiKey) return invoke(cb);
   return invoke(cb);
 };
