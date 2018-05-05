6cab3262de1841a68f19fbc7f733b08d8c98628f
Rowland Henshaw <rowland.henshaw@andela.com>
2018-05-05 19:38:51 +0100

    setup dynamo to test api-gateway (#5)

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import config from './config';
+import * as db from './lib/db';
 
 const welcomeMessage = {
   message: 'Welcome to aws lambda'
@@ -6,11 +7,25 @@ const welcomeMessage = {
 
 const apiKey = config.get('lambdaAccessKey');
 
-const invoke = cb => cb(null, { welcomeMessage });
+const invoke = (event, cb) => {
+  const {
+    action,
+    resource,
+    body = {}
+  } = event;
+
+  switch (action || resource) {
+
+  case 'create':
+    return {};
+
+  default:
+    return Promise.reject(Error('Invalid request'));
+  }
+};
 
 module.exports.handler = (event, context, cb) => {
   const { accessKey } = event;
 
-  if (accessKey === apiKey) return invoke(cb);
-  return invoke(cb);
+  if (accessKey === apiKey) invoke(event).then(response => cb(null, response));
 };
